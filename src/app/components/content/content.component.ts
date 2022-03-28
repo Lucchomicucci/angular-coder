62fcc73021896ea698885942bdd813f018d03f16
Luciano Micucci <luchomicucci@gmail.com>
2022-03-27 21:10:56 -0300

    Entrega 2

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-
+  arrayDeAlumnos: Array<string> = ["Jose", "Luciano", "Juan", "Lucas", "Nazarena"]
   constructor() { }
 
   ngOnInit(): void {
